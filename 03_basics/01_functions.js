bc4fb8ea65d10e0f8eaedb73496acea9fcea3ccf
Pallavi Shukla <pallavi04pallavi@gmail.com>
2024-05-05 17:02:27 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,10 @@
+function addTwoNum(number1, number2) {
+    // console.log(number1 + number2);
+    console.log("Hii JS");
+    return number1 + number2;
+    // console.log(Hello Ji);   UNREACHABLE CODE
+}
+
+// addTwoNum(2, 4);
+const result = addTwoNum(3, 4);
+console.log(result);
